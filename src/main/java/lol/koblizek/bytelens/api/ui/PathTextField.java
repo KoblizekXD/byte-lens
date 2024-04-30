106e94ba78e57d4286fc881f03eb96bcb61130eb
KoblizekXD <janprokupek@centrum.cz>
2024-04-30 20:41:24 +0000

    Placeholder support for PathTextField

--- src/main/java/lol/koblizek/bytelens/api/ui/PathTextField.java
+++ src/main/java/lol/koblizek/bytelens/api/ui/PathTextField.java
@@ -4,6 +4,7 @@ import com.sun.javafx.scene.EventHandlerProperties;
 import javafx.beans.property.ObjectProperty;
 import javafx.beans.property.SimpleObjectProperty;
 import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
 import javafx.event.EventHandler;
 import javafx.geometry.Pos;
 import javafx.scene.control.TextField;
@@ -13,9 +14,13 @@ import javafx.scene.layout.StackPane;
 public class PathTextField extends StackPane {
 
     private ObjectProperty<EventHandler<? super MouseEvent>> onIconClick;
+    private StringProperty placeholder = new SimpleStringProperty();
 
     public PathTextField() {
-        getChildren().add(new TextField());
+        super();
+        TextField t = new TextField();
+        t.promptTextProperty().bind(placeholder);
+        getChildren().add(t);
         IconButton i = new IconButton(new SimpleStringProperty("/lol/koblizek/bytelens/resources/wallet.png"));
         getChildren().add(i);
         setAlignment(i, Pos.CENTER_RIGHT);
@@ -29,5 +34,13 @@ public class PathTextField extends StackPane {
 
     public void setOnIconClick(EventHandler<? super MouseEvent> onIconClick) {
         this.onIconClick.set(onIconClick);
+    }
+
+    public void setPlaceholder(String placeholder) {
+        this.placeholder.set(placeholder);
+    }
+
+    public String getPlaceholder() {
+        return placeholder.get();
     }
 }
