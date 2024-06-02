f33fbb912fdf035af4662efed13347d0a1e1e585
KoblizekXD <janprokupek@centrum.cz>
2024-06-02 09:28:36 +0200

    MenuBar icons on both sides

--- src/main/java/lol/koblizek/bytelens/core/controllers/MainViewController.java
+++ src/main/java/lol/koblizek/bytelens/core/controllers/MainViewController.java
@@ -13,7 +13,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
-// This code will have to be eventually rewritten to be more effective and more understandable
+// This code will have to be "eventually" rewritten to be more effective and more understandable
 
 public class MainViewController extends Controller {
 
