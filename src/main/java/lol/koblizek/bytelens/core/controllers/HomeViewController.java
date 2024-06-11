e08862d6e92642277a876c848f1bba31990a7ce7
KoblizekXD <janprokupek@centrum.cz>
2024-06-11 22:21:21 +0200

    2 buttons

--- src/main/java/lol/koblizek/bytelens/core/controllers/HomeViewController.java
+++ src/main/java/lol/koblizek/bytelens/core/controllers/HomeViewController.java
@@ -1,8 +1,12 @@
 package lol.koblizek.bytelens.core.controllers;
 
+import javafx.scene.control.ListView;
+import lol.koblizek.bytelens.api.DefaultProject;
 import lol.koblizek.bytelens.core.ByteLens;
 
 public class HomeViewController extends Controller {
+
+    public ListView<String> projectListing;
 
     public HomeViewController(ByteLens byteLens) {
         super(byteLens);
@@ -10,6 +14,8 @@ public class HomeViewController extends Controller {
 
     @Override
     public void initialize() {
-
+        for (DefaultProject project : getByteLens().getProjects()) {
+            projectListing.getItems().add(project.getName());
+        }
     }
 }
