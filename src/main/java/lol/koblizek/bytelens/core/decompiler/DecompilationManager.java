0320933fb57d9a262147243c11c2a0d6a536dc9c
KoblizekXD <janprokupek@centrum.cz>
2024-07-03 21:03:25 +0200

    Caching/Downloading operations for decompilers

--- src/main/java/lol/koblizek/bytelens/core/decompiler/DecompilationManager.java
+++ src/main/java/lol/koblizek/bytelens/core/decompiler/DecompilationManager.java
@@ -2,11 +2,15 @@ package lol.koblizek.bytelens.core.decompiler;
 
 import lol.koblizek.bytelens.core.ByteLens;
 import lol.koblizek.bytelens.core.utils.MavenMetadata;
+import lol.koblizek.bytelens.core.utils.StringUtils;
+import org.apache.commons.io.FileUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Optional;
 
 /**
  * Class used to manage decompiler versioning.
@@ -15,7 +19,7 @@ import java.nio.file.Files;
 public class DecompilationManager {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(DecompilationManager.class);
-    private static final String[] PROVIDERS = {"vineflower", "cfr"};
+    private static final String[] PROVIDERS = {"org.vineflower:vineflower", "cfr"};
 
     public static DecompilationManager init(ByteLens inst) {
         return new DecompilationManager(inst);
@@ -46,7 +50,7 @@ public class DecompilationManager {
      * @return Array of decompilers available for download from remote repositories
      */
     public String[] getAvailableDecompilers() {
-        var metadata = MavenMetadata.fromMavenCentral("org.vineflower:vineflower");
+        var metadata = MavenMetadata.fromMavenCentral(PROVIDERS[0]);
         return metadata.versions().stream().map(version -> metadata.artifact().toString() + "-" + version.toString())
                 .toArray(String[]::new);
     }
@@ -61,6 +65,35 @@ public class DecompilationManager {
      */
     public Decompiler selectDecompiler(String decompiler) {
         throw new UnsupportedOperationException("Not implemented yet");
+    }
+
+    /**
+     * @param decompiler Decompiler to download, expected in <name>-<version> format
+     */
+    private void downloadDecompiler(String decompiler) {
+        String url = "https://repo1.maven.org/maven2/" + PROVIDERS[0].replaceAll("[.:]", "/") + "/"
+                + decompiler.split("-")[1] + "/" + decompiler + ".jar";
+        var cache = ByteLens.getCache().resolve("decompilers/" + decompiler + ".jar");
+        copyToPath(url, cache);
+    }
+
+    private Path copyToPath(String path, Path output) {
+        try {
+            var url = StringUtils.toRemoteURL(path);
+            if (url == null) throw new IOException();
+            FileUtils.copyURLToFile(url, output.toFile());
+            return output;
+        } catch (IOException e) {
+            LOGGER.error("Failed to fetch from remote: {}", path, e);
+            return null;
+        }
+    }
+
+    private Optional<Path> selectDecompilerFromCache(String decompiler) {
+        var cached = ByteLens.getCache().resolve("decompilers/" + decompiler + ".jar");
+        if (Files.exists(cached))
+            return Optional.of(cached);
+        else return Optional.empty();
     }
 
     public static String[] getProviders() {
